37fa693e7c879f26ed7030487c9c4964dab0f5dd
bradley guthrie <guppy96@gmail.com>
2021-05-18 06:42:14 -0500

    module will not run

--- hello/lib/mix/tasks/hello.ex
+++ hello/lib/mix/tasks/hello.ex
@@ -4,6 +4,8 @@ defmodule Mix.Tasks.Hello do
   @shortdoc "Simply calls the Hello.say/0 function."
   def run(_) do
     # calling our Hello.say() function from earlier
+    Mix.Task.run("app.start")
+
     Hello.say()
   end
 end
