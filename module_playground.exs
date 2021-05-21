04b07b1d50d21f401dfa69234ac1558ae62fae40
bradley guthrie <guppy96@gmail.com>
2021-05-20 20:46:18 -0500

    wotking through import alias and require directives

--- module_playground.exs
+++ module_playground.exs
@@ -2,6 +2,10 @@ defmodule ModulePlayground do
   import IO, only: [puts: 1]
   import Kernel, except: [inspect: 1]
 
+  alias ModulePlayground.Misc.Util.Math, as: MyMath
+
+  require Integer
+
   def say_here do
     # IO.puts("i'm here")
     inspect(" i'm here")
@@ -11,5 +15,9 @@ defmodule ModulePlayground do
     puts("Starting Output")
     puts(param1)
     puts("Ending output")
+  end
+
+  def print_sum do
+    MyMath.add(1, 2)
   end
 end
