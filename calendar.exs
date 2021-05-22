6490ee9afa2979ad6149669629b20ab8723fefc3
bradley guthrie <guppy96@gmail.com>
2021-05-22 06:32:14 -0500

    working with gaurd example

---
+++ calendar.exs
@@ -0,0 +1,7 @@
+defmodule Sample.calendar do
+
+  def is_leap_year(year) when rem(year, 400) == 0, do: true
+  def is_leap_year(year) when rem(year, 100) == 0, do: true
+  def is_leap_year(year) when rem(year, 4) == 0, do: true
+  def is_leap_year(year), do:  do: false
+end
