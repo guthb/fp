364139e7783314c95f7c35d8c6c16b35dd97f719
bradley guthrie <guppy96@gmail.com>
2021-05-23 17:27:37 -0500

    parameters for lists

--- sample_enum.exs
+++ sample_enum.exs
@@ -1,7 +1,15 @@
 defmodule Sample.Enum do
-  def first(list) when length(list) == 0, do: nil
+  # def first(list) when length(list) == 0, do: nil
+  def first(list, val \\ nil)
+  def first ([head | _], _), do: val
+  # def first ([head | ]), do: head
+
+  def first([], val), do: val
+
+  def add (list, val \\ 0 ) do
+    [val | list]
+  end
 
 
-  def first([head | _]) do: head
 
 end
