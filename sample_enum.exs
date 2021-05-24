eafe2889763c783afffe35c519fe26a844ac840a
bradley guthrie <guppy96@gmail.com>
2021-05-24 06:30:46 -0500

    function example

--- sample_enum.exs
+++ sample_enum.exs
@@ -1,15 +1,21 @@
 defmodule Sample.Enum do
+
   # def first(list) when length(list) == 0, do: nil
   def first(list, val \\ nil)
+
   def first ([head | _], _), do: val
+
   # def first ([head | ]), do: head
 
   def first([], val), do: val
 
   def add (list, val \\ 0 ) do
+    trace(val)
     [val | list]
   end
 
-
+  defp  trace(sting) do
+    IO.puts("the value passed in was #{string}")
+  end
 
 end
