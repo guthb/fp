ff7b3795fb48b1baf6b2eec5263bd229c55678f1
bradley guthrie <guppy96@gmail.com>
2021-05-26 22:12:01 -0500

    using con

--- sample_dayofweek.exs
+++ sample_dayofweek.exs
@@ -25,6 +25,27 @@ defmodule Sample.DayOfWeek do
                 end
             end
         end
+  end
+
+  def day_appreviation2(day) def do
+    cond do
+      day == :Monday -> "M"
+      day == :Tuesday -> "T"
+      day == :Wednesday -> "W"
+      day == :Thursday -> "R"
+      day == :Saturday -> "Sa"
+      day == :Sunday -> "Su"
+      true -> "Invalid day"
     end
   end
+
+  def day_abbreviation3(:Monday), do:"M"
+  def day_abbreviation3(:Monday), do:"T"
+  def day_abbreviation3(:Monday), do:"W"
+  def day_abbreviation3(:Monday), do:"R"
+  def day_abbreviation3(:Monday), do:"F"
+  def day_abbreviation3(:Monday), do:"Sa"
+  def day_abbreviation3(:Monday), do:"Su"
+  def day_abbreviation3(), do:"Invalid Day"
+
 end
