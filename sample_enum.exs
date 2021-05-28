6087172dceeef6fb2659829a2f4eb7338149715b
bradley guthrie <guppy96@gmail.com>
2021-05-28 17:39:47 -0500

    recursion

--- sample_enum.exs
+++ sample_enum.exs
@@ -1,21 +1,24 @@
 defmodule Sample.Enum do
 
   # def first(list) when length(list) == 0, do: nil
-  def first(list, val \\ nil)
-
-  def first ([head | _], _), do: val
+  def first([]), do: nil
+  def first ([head | _]), do: head
 
   # def first ([head | ]), do: head
 
-  def first([], val), do: val
+  # def first([], val), do: val
 
-  def add (list, val \\ 0 ) do
-    trace(val)
-    [val | list]
+  # def add (list, val \\ 0 ) do
+  #   trace(val)
+  #   [val | list]
+  # end
+
+  # defp  trace(sting) do
+  #   IO.puts("the value passed in was #{string}")
+  # end
+
+  def map([], _), do: []
+  def map([hd | tl], f) do
+    [f.(hd) | map(tl, f)]
   end
-
-  defp  trace(sting) do
-    IO.puts("the value passed in was #{string}")
-  end
-
 end
