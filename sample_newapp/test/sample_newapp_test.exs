90c0356563442da70a5e68304a65b3c2a2d43391
bradley guthrie <guppy96@gmail.com>
2021-06-04 07:59:42 -0500

    starting with tests

--- sample_newapp/test/sample_newapp_test.exs
+++ sample_newapp/test/sample_newapp_test.exs
@@ -5,4 +5,13 @@ defmodule SampleNewappTest do
   test "greets the world" do
     assert SampleNewapp.hello() == :world
   end
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+
+  # @tag watching: true
+  test "another test" do
+    assert 2 + 2 == 4
+  end
 end
