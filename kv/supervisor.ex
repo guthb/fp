dd5d553513c2369e965b61e59f62ab0e4b7c189d
bradley guthrie <guppy96@gmail.com>
2021-06-17 15:41:49 -0500

    starting over to see if i can get this working

--- kv/supervisor.ex
+++ kv/supervisor.ex
@@ -8,6 +8,7 @@ defmodule KV.Supervisor do
   @impl true
   def init(:ok) do
     children = [
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one},
       {KV.Registry, name: KV.Registry}
     ]
 
