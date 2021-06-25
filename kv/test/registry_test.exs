c7d3b095c6f4ee705f1e1b3d30c7c973502fad12
bradley guthrie <guppy96@gmail.com>
2021-06-24 21:22:40 -0500

    new tests

--- kv/test/registry_test.exs
+++ kv/test/registry_test.exs
@@ -20,6 +20,24 @@ defmodule KV.RegistryTest do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+    # assert KV.Registry.lookup(registry, "shopping") == :error
+  # end
+
+  # Do a call to ensure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
+
+  test "removes bucket on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Stop the bucket with non-normal reason
+    Agent.stop(bucket, :shutdown)
+
+    # Do a call to ensure the registry processed the DOWN message
+    _ = KV.Registry.create(registry, "bogus")
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
 end
