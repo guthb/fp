ccae933a07ba851ef9d45a9ec9aeae4329233273
bradley guthrie <guppy96@gmail.com>
2021-06-28 19:36:09 -0500

    ready for with

--- kv_umbrella/apps/kv/lib/command.ex
+++ kv_umbrella/apps/kv/lib/command.ex
@@ -11,6 +11,10 @@ defmodule KVServer.Command do
   def parse(line) do
     case String.split(line) do
       ["CREATE", bucket] -> {:ok, {:create, bucket}}
+      ["GET", bucket, key] -> {:ok, {:get, bucket, key}}
+      ["PUT", bucket, key, value] -> {:ok, {:put, bucket, key, value}}
+      ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
+      _ -> {:error, :unknown_command}
     end
   end
 end
