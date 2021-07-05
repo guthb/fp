17d46f567d5a6b88116bb89fd90d570fb617565a
bradley guthrie <guppy96@gmail.com>
2021-07-04 19:56:35 -0500

    testing proxy error

--- kv_umbrella/apps/kv/mix.exs
+++ kv_umbrella/apps/kv/mix.exs
@@ -27,8 +27,8 @@ defmodule KV.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dep_from_hexpm, "~> 0.3.0"},
-      {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "ssh://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
