22549a5f6a48f1818f10567c9f351ce036e4f11e
bradley guthrie <guppy96@gmail.com>
2021-07-31 16:14:25 -0500

    need to understand what all the _raise execption types are

--- koans/lib/koans/12_pattern_matching.ex
+++ koans/lib/koans/12_pattern_matching.ex
@@ -36,11 +36,11 @@ defmodule PatternMatching do
   koan "Maps support partial pattern matching" do
     %{make: make} = %{type: "car", year: 2016, make: "Honda", color: "black"}
 
-    assert make == ___
+    assert make == "Honda"
   end
 
   koan "Lists must match exactly" do
-    assert_raise ___, fn ->
+    assert_raise MatchError, "no match of right hand side value: [1, 2, 3]", fn ->
       [a, b] = [1, 2, 3]
     end
   end
@@ -48,7 +48,7 @@ defmodule PatternMatching do
   koan "So does the keyword lists" do
     kw_list = [type: "car", year: 2016, make: "Honda"]
     [_type | [_year | [tuple]]] = kw_list
-    assert tuple == {___, ___}
+    assert tuple == {:make, "Honda"}
   end
 
   koan "The pattern can make assertions about what it expects" do
