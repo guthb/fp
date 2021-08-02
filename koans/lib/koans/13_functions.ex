4f34a06b486ab4787cf9542cf3e0c6bd17068fb7
bradley guthrie <guppy96@gmail.com>
2021-08-02 15:45:06 -0500

    enjoying the functions

--- koans/lib/koans/13_functions.ex
+++ koans/lib/koans/13_functions.ex
@@ -14,15 +14,15 @@ defmodule Functions do
   def multiply(a, b), do: a * b
 
   koan "Single line functions are cool, but mind the comma and the colon!" do
-    assert 6 == multiply(2, ___)
+    assert 6 == multiply(2, 3)
   end
 
   def first(foo, bar), do: "#{foo} and #{bar}"
   def first(foo), do: "Only #{foo}"
 
   koan "Functions with the same name are distinguished by the number of arguments they take" do
-    assert first("One", "Two") == ___
-    assert first("One") == ___
+    assert first("One", "Two") == "One and Two"
+    assert first("One") == "Only One"
   end
 
   def repeat_again(message, times \\ 5) do
@@ -30,8 +30,8 @@ defmodule Functions do
   end
 
   koan "Functions can have default argument values" do
-    assert repeat_again("Hello ") == ___
-    assert repeat_again("Hello ", 2) == ___
+    assert repeat_again("Hello ") == "Hello Hello Hello Hello Hello "
+    assert repeat_again("Hello ", 2) == "Hello Hello "
   end
 
   def sum_up(thing) when is_list(thing), do: :entire_list
