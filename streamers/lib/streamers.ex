a7e3bb23bd5e9486b479ebec9d7e91fef7c2cebd
bradley guthrie <guppy96@gmail.com>
2021-08-13 18:37:05 -0500

    changed to private function and testing

--- streamers/lib/streamers.ex
+++ streamers/lib/streamers.ex
@@ -19,21 +19,13 @@ defmodule Streamers do
   def find_index(directory) do
     files = Path.join(directory, "*.m3u8")
 
-    Enum.find(Path.wildcard(files), fn file ->
-      is_index?(file)
-    end)
+    Enum.find(Path.wildcard(files), is_index?(&1)
   end
 
-  def is_index?(file) do
-    File.open(file, fn
-      "#somethinginfile\n#anotherthinginfile" <> _ ->
-        true
-
-      _ ->
-        false
-
-      contents ->
-        nil
+  defp is_index?(file) do
+    File.open! file, fn
+      "#somethinginfile\n#anotherthinginfile" <> _ -> true
+      _ -> false
     end)
   end
 end
