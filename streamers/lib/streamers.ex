f6cf9e13292f741af6c4fa006b47b845028dfe68
bradley guthrie <guppy96@gmail.com>
2021-08-15 09:10:34 -0500

    reading file contents with streaming

--- streamers/lib/streamers.ex
+++ streamers/lib/streamers.ex
@@ -1,4 +1,5 @@
 defmodule Streamers do
+  defrecord M3U8, program_id: nil, file bandwith: nil
   @moduledoc """
   Documentation for `Streamers`.
   """
@@ -23,7 +24,24 @@ defmodule Streamers do
     files = Path.join(directory, "*.m3u8")
 
     if file = Enum.find(Path.wildcard(files), is_index?(&1)) do
-      Path.basename(file)
+      file
+    end
+  end
+
+  def extract_m3u8(index_file) do
+    File.open! file_index, fn(pid) ->
+      #discards #EXTM3U
+      IO.readline(pid)
+      do_extract_mu3u8(pid, [])
+    end
+  end
+
+  defp do_extrace_m3u8(pid, acc)do
+    case IO.readline(pid) do
+      :eof -> figure_this_out
+      stream_inf ->
+        path = IO.readline(pid)
+        do_extrace_m3u8(pid, stream_inf, path, acc)
     end
   end
 
--- streamers/test/streamers_test.exs
+++ streamers/test/streamers_test.exs
@@ -1,17 +1,29 @@
 defmodule StreamersTest do
   use ExUnit.Case, async:true
+
   doctest Streamers
+  alias Streamers.M3U8, as M3U8
+
+  @index_file "test/fixtures/emberjs/somefile.m3u8"
 
   test "greets the world" do
     assert Streamers.hello() == :world
   end
 
   test "find index file in a directory" do
-    assert Streamers.find_index("test/fixtures/emberjs") ==
-             "somefile.m3u8"
+    assert Streamers.find_index("test/fixtures/emberjs") == @index_file
+
   end
 
   test "returns nill if not availbe for index file" do
     assert Streamers.find_index("test/fixtures/not_available") == nil
+  end
+
+  test "extracts m3u8 from index file" do
+    m3u8 = Streamers.extrac_m3u8(@index_file)
+    assert Enum.first(m3u8)==
+      M3U8[program_id: 1, bandwith: 110000, path: "test/fixtures/emberjs/somefile.m3u8"]
+
+
   end
 end
