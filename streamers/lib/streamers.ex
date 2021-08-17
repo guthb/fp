1eb9bce3d189536c2085091e6bd40fe6eaf7310f
bradley guthrie <guppy96@gmail.com>
2021-08-16 20:12:08 -0500

    arity change

--- streamers/lib/streamers.ex
+++ streamers/lib/streamers.ex
@@ -32,24 +32,25 @@ defmodule Streamers do
     File.open! file_index, fn(pid) ->
       #discards #EXTM3U
       IO.readline(pid)
-      do_extract_mu3u8(pid, [])
+      do_extract_mu3u8(pid, Path.dirname(index_file),[])
     end
   end
 
-  defp do_extrace_m3u8(pid, acc)do
+  defp do_extrace_m3u8(pid, dir, acc)do
     case IO.readline(pid) do
       :eof -> figure_this_out
       stream_inf ->
         path = IO.readline(pid)
-        do_extrace_m3u8(pid, stream_inf, path, acc)
+        do_extrace_m3u8(pid, dir, stream_inf, path, acc)
     end
   end
 
-  defp  do_extract_m3u8(pid, stream_inf, path, acc)do
+  defp  do_extract_m3u8(pid, dir, stream_inf, path, acc)do
     << "stuff", program_id, ",BANDWIDTH=", bandwidth :: binary >> == steam_inf
-    program_id = binary_to_integer(<<program_id>>)
+    # program_id = binary_to_integer(<<program_id>>)
+    path = Path.join(dir, String.strip(path))
     record = M3U8[program_id: program_id -?0, path: String.strip( path), bandwidth: String.strip(bandwidth])
-    do_extract_m3u8(pid, [record|acc])
+    do_extract_m3u8(pid, dir, [record|acc])
   end
 
   defp is_index?(file) do
