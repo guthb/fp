1706ee037df254800ad0d2654d5344dcade203a7
bradley guthrie <guppy96@gmail.com>
2021-08-24 08:52:40 -0500

    server now serving content

--- firebirdplayground/rsvp_umbrella/config/config.exs
+++ firebirdplayground/rsvp_umbrella/config/config.exs
@@ -9,6 +9,17 @@
 # move said applications out of the umbrella.
 import Config
 
+config :rsvp_web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :rsvp_web, RsvpWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "kqWRRQHP00HLvs64uJlbyu0t0NtzB4mKvW/ibsyM4ZudEJc4BUIMunhi8JEoEr2e",
+  render_errors: [view: RsvpWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: RsvpWeb.PubSub,
+  live_view: [signing_salt: "KuEImtYd"]
+
 # Sample configuration:
 #
 #     config :logger, :console,
@@ -16,3 +27,15 @@ import Config
 #       format: "$date $time [$level] $metadata$message\n",
 #       metadata: [:user_id]
 #
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
