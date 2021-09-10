05d8b45f6ef536241952a4054c2c0e2d8d0d411b
bradley guthrie <guppy96@gmail.com>
2021-09-09 21:23:05 -0500

    setting up route

--- phoenixphun/newphx/lib/newphx_web/router.ex
+++ phoenixphun/newphx/lib/newphx_web/router.ex
@@ -2,21 +2,22 @@ defmodule NewphxWeb.Router do
   use NewphxWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", NewphxWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    get("/events/:id", EventController, :show)
   end
 
   # Other scopes may use custom stacks.
@@ -35,8 +36,8 @@ defmodule NewphxWeb.Router do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: NewphxWeb.Telemetry
+      pipe_through(:browser)
+      live_dashboard("/dashboard", metrics: NewphxWeb.Telemetry)
     end
   end
 end
