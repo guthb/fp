f11e1184b9fef2ccd05b527de3f0ba04a5ca548d
bradley guthrie <guppy96@gmail.com>
2021-09-10 19:55:38 -0500

    building out controller

--- phoenixphun/newphx/lib/newphx_web/controllers/event_controller.ex
+++ phoenixphun/newphx/lib/newphx_web/controllers/event_controller.ex
@@ -1,2 +1,10 @@
 defmodule NewphxWeb.EventController do
+  use NewphxWeb.Web, :controller
+
+  def show(conn, _parms) do
+    # text conn, "Events #{parms["id"]}"
+    render conn, "details.html", event: "Events #{parms["id"]}"
+  end
+
+
 end
