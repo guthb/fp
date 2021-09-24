f37200b0d000c34939d6797c4b61fce6ae74f050
bradley guthrie <guppy96@gmail.com>
2021-09-23 20:33:32 -0500

    creating a cookie

--- phoenixphun/newphx/lib/newphx_web/controllers/login_controller.ex
+++ phoenixphun/newphx/lib/newphx_web/controllers/login_controller.ex
@@ -4,8 +4,11 @@ defmodule NewphxWeb.LoginController do
   def index(conn, _) do
     render conn, "login.html"
   end
-
-  def login(conn, params) do
-
+  #do not use in production
+  def login(conn, %{"login" => %{"username" => name}}) do
+    expiration = 60*60*24*7
+    conn
+    |> Plug.Conn.put_resp_cookie("user_name", name, max_age: expiration)
+    |> redirect(to: "/")
   end
 end
