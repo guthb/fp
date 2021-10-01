5110d61b5a275a1c02361d0d9899ad871b6c5cd6
bradley guthrie <guppy96@gmail.com>
2021-10-01 07:17:08 -0500

    some more adds

---
+++ phoenixphun/newphx/lib/newphx_web/channels/event_channel.ex
@@ -0,0 +1,12 @@
+defmodule NewphxWeb.EventChannel do
+  use Phoenix.Channel
+
+  def join("event:" <> event_id, _message, socket)  when event_id <= 0 do
+    {:error, %(reason: "invalid event id")}
+  end
+
+  def join("event:" <> event_id, _message, socket) do
+    {:ok, socket}
+  end
+
+end
